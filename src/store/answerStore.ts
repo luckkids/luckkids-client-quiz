b2e8bfb16ef8af1769a58acdeccffb21e81fafb7
Gina Park <koyrkr@gmail.com>
2025-11-21 21:27:02 +0900

    Add answerStore using Zustand for managing quiz answers and user nickname

---
+++ src/store/answerStore.ts
@@ -0,0 +1,51 @@
+import { create } from 'zustand';
+
+export interface Answer {
+  quizIndex: number;
+  answerIndex: number;
+}
+
+interface AnswerState {
+  nickname: string | null;
+  answers: Answer[];
+  setNickname: (nickname: string) => void;
+  setAnswer: (quizIndex: number, answerIndex: number) => void;
+  getAnswer: (quizIndex: number) => number | undefined;
+  clearAnswers: () => void;
+}
+
+export const useAnswerStore = create<AnswerState>((set, get) => ({
+  nickname: null,
+  answers: [],
+
+  setNickname: (nickname: string) => {
+    set({ nickname });
+  },
+
+  setAnswer: (quizIndex: number, answerIndex: number) => {
+    const { answers } = get();
+    const existingAnswerIndex = answers.findIndex(
+      (answer) => answer.quizIndex === quizIndex
+    );
+
+    if (existingAnswerIndex !== -1) {
+      // 같은 퀴즈에 대한 답변이 있으면 덮어쓰기
+      const newAnswers = [...answers];
+      newAnswers[existingAnswerIndex] = { quizIndex, answerIndex };
+      set({ answers: newAnswers });
+    } else {
+      // 새로운 답변 추가
+      set({ answers: [...answers, { quizIndex, answerIndex }] });
+    }
+  },
+
+  getAnswer: (quizIndex: number) => {
+    const { answers } = get();
+    const answer = answers.find((answer) => answer.quizIndex === quizIndex);
+    return answer?.answerIndex;
+  },
+
+  clearAnswers: () => {
+    set({ nickname: null, answers: [] });
+  },
+}));
