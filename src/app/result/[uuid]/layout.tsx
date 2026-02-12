d7870f5acf084e5a244f1545ebe2c6bb01de4881
gil <longlongroad@naver.com>
2026-02-12 16:53:23 +0900

    [modify] 초기화 방식 변경

--- src/app/result/[uuid]/layout.tsx
+++ src/app/result/[uuid]/layout.tsx
@@ -1,9 +1,15 @@
 import Script from "next/script";
 import React from "react";
 
+const KAKAO_JS_KEY = process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY;
+
 export default function ResultLayout({children}:{ children: React.ReactNode;}){
     return <>
-        <Script src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.9/kakao.min.js" integrity="sha384-JpLApTkB8lPskhVMhT+m5Ln8aHlnS0bsIexhaak0jOhAkMYedQoVghPfSpjNi9K1" crossOrigin="anonymous" strategy="beforeInteractive"></Script>
+        <Script src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.9/kakao.min.js" integrity="sha384-JpLApTkB8lPskhVMhT+m5Ln8aHlnS0bsIexhaak0jOhAkMYedQoVghPfSpjNi9K1" crossOrigin="anonymous" strategy="beforeInteractive" onLoad={()=>{
+            if (window.Kakao && !window.Kakao.isInitialized()) {
+                window.Kakao.init(KAKAO_JS_KEY);
+            }
+        }}></Script>
         {children}
     </>
 }
--- src/app/result/[uuid]/page.tsx
+++ src/app/result/[uuid]/page.tsx
@@ -10,7 +10,6 @@ import { useEffect, useState } from 'react';
 import { Constants } from '@/constants';
 import { TQuizType } from '@/interface/interface';
 
-const KAKAO_JS_KEY = process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY;
 const KAKAO_TMP_ID = process.env.NEXT_PUBLIC_KAKAO_TEMPLATE_ID;
 
 const S = {
@@ -159,12 +158,6 @@ export default function Result() {
     const { uuid } = useParams();
     const navigate = useRouter();
   const [result, setResult] = useState<IResponse>();
-
-  useEffect(()=>{
-      if(window.Kakao && !window.Kakao.isInitialized()){
-          window.Kakao.init(KAKAO_JS_KEY);
-      }
-  },[window.Kakao])
 
     const KaKaoShare = (text:string) =>{
         window.Kakao.Share.sendCustom({
